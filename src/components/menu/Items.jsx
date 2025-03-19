16c9adb0d2e0f8363e97ddcef0e3a58f7dad85c1
Diptesh Chakraborty <diptesh.chakraborty@in.ey.com>
2025-03-19 11:56:49 +0530

    cart total2

--- src/components/menu/Items.jsx
+++ src/components/menu/Items.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Item from "./Item";
 import { useOutletContext } from 'react-router-dom';
 
 export default function Items() {
 
     const { selectedCategory, filteredMenuItems, menuItems, cartItems, setCartItems } = useOutletContext();
+
+    // Save cart items to local storage whenever cartItems change
+      useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      }, [cartItems]);
 
     return (
         <div>
